a779b3357ee726e60e874a8f6f72c79ed4099136
j-m-smith426 <joabsmith4226@gmail.com>
2021-08-27 17:51:56 -0400

    oops

--- src/Axios/AxiosConfig.ts
+++ src/Axios/AxiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let config = axios.create({
-  baseURL: "http://192.168.0.96:3000/api/VideoGames",
+  baseURL: "http://localhost:3000/api/VideoGames",
 });
 
 export default config;
